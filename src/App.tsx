039e02a860131dd3fe9e6988b3623d29cf2ae0ce
Prince Rathod <rathodprince411@gmail.com>
2026-01-17 13:44:19 +0530

    shadcn and tanstack query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <main className="bg-[#f9f9f9] flex flex-col items-center min-h-screen">
+      <Navbar />
+      <h1 className="text-black text-4xl font-bold">Hello, CA Monk!</h1>
+    </main>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='top-0 left-0 right-0 w-full h-12 border-2 border-black shadow-md'>Navbar</nav>
+  )
+}
+
+export default Navbar
